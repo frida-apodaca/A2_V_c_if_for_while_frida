8fcf0f57184529b3130f21075cc9ce743acc2fdb
frida-apodaca <a.22308051281136@cbtis128.edu.mx>
2024-09-06 09:33:04 -0600

    Add files via upload
    subí dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,22 @@
+"""frida sofia
+trabajando cadenas."""
+
+
+nombre="Frida"
+print(nombre[0])
+print(nombre)
+
+Misnovias=["f","r","i"]
+for chicas in Misnovias:
+    print(chicas)
+    
+    print("otro ejemplo")
+    b = "frida apodaca"
+print(b[2:5])
+a = "frida apodaca"
+print(a.upper())
+print("concatenar")
+a = "maria"
+b = "Perez"
+c = a + " " + b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,17 @@
+edad=17
+import math
+
+nombre="frida apodaca"
+print("tu edad es",edad)
+print("te llamas ",nombre)
+print("-- tipo de variables --")
+x = str(7) #x will de 3
+y = int(3.1416) #y will de 3
+z = float(7)
+print(x)
+print(y)
+print(z)
+print("-- funciones matematicas --")
+x = math.sqrt(64)
+
+print(x)
